204fbbb7430b0d5c53b3b485e0db97daa41c0f0a
TAKESUSU <y.takesue0325@gmail.com>
2019-07-05 01:06:10 +0900

    Ver1

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+// 共通で使うものを別ファイルにしておきましょう。
+
+// DB接続関数（PDO）
+function db_conn()
+{
+    $dbname = 'gsf_d03_db12';
+    $db = 'mysql:dbname=' . $dbname . ';charset=utf8;port=3306;host=localhost';
+    $user = 'root';
+    $pwd = '';
+    try {
+        return new PDO($db, $user, $pwd);
+    } catch (PDOException $e) {
+        exit('dbError:' . $e->getMessage());
+    }
+}
+
+// SQL処理エラー
+function errorMsg($stmt)
+{
+    $error = $stmt->errorInfo();
+    exit('ErrorQuery:' . $error[2]);
+}
+
+// SESSIONチェック＆リジェネレイト
+function chk_ssid()
+{
+    if (!isset($_SESSION['chk_ssid']) || $_SESSION['chk_ssid'] != session_id()) {
+        // ログイン失敗時の処理（ログイン画面に移動）
+        header('Location: login.php');
+    } else {
+        // ログイン成功時の処理（一覧画面に移動）
+        session_regenerate_id(true);
+        $_SESSION['chk_ssid'] = session_id();
+    }
+}
+
+// menuを決める
+function menu()
+{
+    $menu = '<li class="nav-item"><a class="nav-link" href="index.php">ブックマーク</a></li><li class="nav-item"><a class="nav-link" href="select.php">データ一覧</a></li>';
+    $menu .= '<li class="nav-item"><a class="nav-link" href="logout.php">ログアウト</a></li>';
+    return $menu;
+}
